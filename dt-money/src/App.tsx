208be16891a56c5f73add2b70c71d745d9be3ec9
Brenda <brendaprofiro@gmail.com>
2021-04-01 13:50:53 -0300

    chore: Add new components to App.tsx file

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,8 +1,41 @@
+import { useState } from 'react';
+
+import Modal from 'react-modal';
+
+import { TransactionsProvider } from './hooks/useTransactions';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
+
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  };
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </TransactionsProvider>
   );
 }
 
