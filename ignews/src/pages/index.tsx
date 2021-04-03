da34a4a52d0fc65c0fc3e08ca705dc2255888b2c
Brenda <brendaprofiro@gmail.com>
2021-04-03 16:04:26 -0300

    chore: Create initial home page structure

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from 'next/head';
 
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | Ignews</title>
       </Head>
-      <h1>oi</h1>
+    
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month.</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
