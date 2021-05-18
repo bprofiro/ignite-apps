93bfcc41fc65eb16301335924b4dd7abe81c25d7
Brenda <brendaprofiro@gmail.com>
2021-05-17 22:00:17 -0300

    chore: Change home styles directory

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,23 +1,36 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+import styles from '../styles/pages/home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | Ignews</title>
       </Head>
     
-      <main>
-        <section>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world</h1>
 
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month.</span>
+            <span>for {product.amount} month.</span>
           </p>
+
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
@@ -25,3 +38,22 @@ export default function Home() {
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IcY0jElTtmyiMY79HXHrBda');
+
+  const product = {
+    productId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 *24, // 24 hours
+  }
+};
