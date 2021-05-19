db8f8b6013d5d2f36d65251b3970e4ab49272dfc
Brenda <brendaprofiro@gmail.com>
2021-05-19 20:50:25 -0300

    Update navbar

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -8,8 +10,12 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a className={styles.active}>Home</a>
-          <a href="/posts">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
