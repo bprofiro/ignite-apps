9149fca06c504c6fa67a2af4384167398499bd0d
Brenda <brendaprofiro@gmail.com>
2021-05-19 20:54:42 -0300

    Create posts page

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import { GetStaticProps } from 'next';
+
+import { RichText } from 'prismic-dom';
 import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from '../../styles/pages/posts.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,29 +29,17 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div  className={styles.posts}>
-          <a href="#">
-            <time>12 de Março de 2021</time>
-            <strong>Creating a Monorepo with Lerna and Yarn Workspace</strong>
-            <p>In this guide, you will leran how to create a Monorepo to manage multiple packages with a shared button...</p>
-          </a>
-
-          <a href="#">
-            <time>12 de Março de 2021</time>
-            <strong>Creating a Monorepo with Lerna and Yarn Workspace</strong>
-            <p>In this guide, you will leran how to create a Monorepo to manage multiple packages with a shared button...</p>
-          </a>
-
-          <a href="#">
-            <time>12 de Março de 2021</time>
-            <strong>Creating a Monorepo with Lerna and Yarn Workspace</strong>
-            <p>In this guide, you will leran how to create a Monorepo to manage multiple packages with a shared button...</p>
-          </a>
-
-          <a href="#">
-            <time>12 de Março de 2021</time>
-            <strong>Creating a Monorepo with Lerna and Yarn Workspace</strong>
-            <p>In this guide, you will leran how to create a Monorepo to manage multiple packages with a shared button...</p>
-          </a>
+          {posts.map(post => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>
+                  {post.excerpt}
+                </p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -47,17 +49,29 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const response = await prismic.query(
-    [
-      Prismic.Predicates.at('document.type', 'publication')
-    ],
-    {
-      fetch: ['publication.title', 'publication.content'],
-      pageSize: 100,
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
     }
-  );
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
